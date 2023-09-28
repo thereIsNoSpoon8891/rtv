dce3ae74ffdafd2ffc234b827e4c349bbe4ed5e0
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-09-28 11:27:21 -0700

    explain and clarify schemas and imports

--- backend/models/issue.js
+++ backend/models/issue.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
-const commentSchema = new Schema({// QUESTION: as of now, Comment is defined in 2 places... It works
+const commentSchema = new Schema({// 
     comment: {
         type: String,
         required: true
@@ -47,5 +46,47 @@ const issueSchema = new Schema({
     },
     voters: [String]
 })
+
+/*
+
+ // trying to import and set as array FAILED, ERROR:
+ TypeError: Invalid schema configuration: `model` is not a valid type within the array 
+ `comments`.See https://bit.ly/mongoose-schematypes for a list of valid schema types.
+/////////////////////////////////////////////////////////////////////////////////////////////////
+if you want to import the comment schema:
+
+const commentSchema = require('./comment')
+
+const issueSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imagUrl: String,
+    upvotes: {
+        type: Number,
+        default: 0
+    },
+    downvotes: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'commentSchema
+    '}],
+    voters: [String]
+})
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
 
 module.exports = mongoose.model('Issue', issueSchema)
